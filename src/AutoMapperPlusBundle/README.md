7a736309c31853cdd87a7af01e878909ac2ba6d5
Mark Gerarts <mark.gerarts@gmail.com>
2017-10-12 17:53:12 +0200

    Move the bundle to its own repository

--- src/AutoMapperPlusBundle/README.md
+++
@@ -1,3 +0,0 @@
-# AutoMapperPlusBundle
-
-A Symfony bundle for [AuotoMapper+](https://www.github.com/mark-gerarts/automapper-plus).
--- src/Demo/AutoMapperConfig.php
+++ src/Demo/AutoMapperConfig.php
@@ -7,8 +7,16 @@ use AutoMapperPlus\Configuration\AutoMapperConfigInterface;
 use Demo\Model\Employee\Employee;
 use Demo\Model\Employee\EmployeeDto;
 
+/**
+ * Class AutoMapperConfig
+ *
+ * @package Demo
+ */
 class AutoMapperConfig implements AutoMapperConfiguratorInterface
 {
+    /**
+     * @inheritdoc
+     */
     public function configure(AutoMapperConfigInterface $config): void
     {
         $config->registerMapping(Employee::class, EmployeeDto::class)
